f91ddb569c39be451600d75e70ca71dc610f0727
teboho moloi <125126846+ttsqldeveloper@users.noreply.github.com>
2024-03-19 20:16:43 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+ builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+app.Urls.Add("http://172.21.202.135:5024"); //Ubuntu IP
+app.Urls.Add("localhost"); // 127.0.0.1 IP
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
